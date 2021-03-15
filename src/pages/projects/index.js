5ed17af854846427d3cfb7bcb5f1196d1dac90e3
vbrodar <vedran.brodar@gmail.com>
2021-03-15 10:07:40 +0100

    Migrated to gatsby-plugin-image

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,7 +2,7 @@ import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import "../../styles/main.css"
-import Img from "gatsby-image"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 export default function Projects({ data }) {
   console.log(data)
@@ -19,7 +19,12 @@ export default function Projects({ data }) {
           {projects.map(project => (
             <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
               <div>
-                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
+                <GatsbyImage
+                  image={
+                    project.frontmatter.thumb.childImageSharp.gatsbyImageData
+                  }
+                  alt="Project thumbnails"
+                />
                 <h3>{project.frontmatter.title}</h3>
                 <p>{project.frontmatter.stack}</p>
               </div>
@@ -46,9 +51,7 @@ export const query = graphql`
           title
           thumb {
             childImageSharp {
-              fluid {
-                ...GatsbyImageSharpFluid
-              }
+              gatsbyImageData(layout: CONSTRAINED)
             }
           }
         }
